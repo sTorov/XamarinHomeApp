c35c97bf40371d41774ba523aff6ae7f31504a01
sTorov <sotorov2011@yandex.ru>
2023-02-23 01:02:30 +0300

    x:Static

--- XamarinHomeApp/XamarinHomeApp/LoginPage.xaml.cs
+++ XamarinHomeApp/XamarinHomeApp/LoginPage.xaml.cs
@@ -11,14 +11,43 @@ namespace XamarinHomeApp
 {
     public partial class LoginPage : ContentPage
     {
+        //Константа для текста кнопки
+        public const string BUTTON_TEXT = "Войти";
+        //Переменная счетчика
+        public static int loginCouner = 0;
+
         public LoginPage()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// По клику обрабатываем счетчик и выводим разные сообщения
+        /// </summary>
         private void LoginButton_Clicked(object sender, EventArgs e)
         {
-            loginButton.Text = "Выполняется вход...";
+            #region LoadingData_Example
+
+            //Генерация во время компиляции (инфо находится в файле <ClassName>.g.cs, который находится в папке скомпилированного приложения (./obj/..))
+            //loginButton.Text = "Выполняется вход...";
+
+            //Динамическая генерация XML-кода (снижение производительности)
+            //string xaml = "<Button Text=\"⌛ Выполняется вход...\" />";
+            //loginButton.LoadFromXaml(xaml);
+
+            #endregion
+
+            if (loginCouner == 0)
+                loginButton.Text = "Выполняется вход...";   //Если первая попытка - просто меняем сообщение
+            else if (loginCouner > 5)
+            {
+                loginButton.IsEnabled = false;  //Деактивируем кнопку
+                errorMessage.Text = "Слишком много попыток! Попробуйте позже.";     //Показываем сообщение об ошибке
+            }
+            else
+                loginButton.Text = $"Выполняется вход... Попыток входа: {loginCouner}";     //Изменяем текст кнопки и показываем количество попыток входа
+
+            loginCouner++;  //Увеличиваем счетчик
         }
     }
 }
