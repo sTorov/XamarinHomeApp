cf87dcae33d3236dece7b92f6bdabe2512d15a89
sTorov <sotorov2011@yandex.ru>
2023-03-02 02:18:57 +0300

    XamarinHomeApp.Andriod => Renderers Кастомизация элементов под различные платформы

---
+++ XamarinHomeApp/XamarinHomeApp.Android/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinHomeApp.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace XamarinHomeApp.Droid.Renderers
+{
+    /// <summary>
+    /// Отключаем подчёркивание по умолчанию для элементоа Editor на платформе Android
+    /// </summary>
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        //Данный конструктор нужен только для платформы Android
+        public CustomEditorRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
---
+++ XamarinHomeApp/XamarinHomeApp.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinHomeApp.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace XamarinHomeApp.Droid.Renderers
+{
+    /// <summary>
+    /// Отключаем подчёркивание по умолчанию для элемента Entry на платформе Android
+    /// </summary>
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        //Данный конструктор нужен только для платформы Android
+        public CustomEntryRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
