d2b22fcdade970b48239e3b9388181f883da1933
sTorov <sotorov2011@yandex.ru>
2023-03-02 04:19:37 +0300

    HomeDevice

--- XamarinHomeApp/XamarinHomeApp.UWP/MainPage.xaml.cs
+++ XamarinHomeApp/XamarinHomeApp.UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +23,13 @@ namespace XamarinHomeApp.UWP
             this.InitializeComponent();
 
             LoadApplication(new XamarinHomeApp.App());
+
+            //Изменение размеров стартового окна
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 600);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            //Для сброса стандартного минимального размера окна
+            //ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(200, 100));
         }
     }
 }
---
+++ XamarinHomeApp/XamarinHomeApp/Models/HomeDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinHomeApp.Models
+{
+    public class HomeDevice
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+        public HomeDevice(string name, string image = null, string description = null) => 
+            (Id, Name, Image, Description) = (new Guid(), name, image, description);
+    }
+}
