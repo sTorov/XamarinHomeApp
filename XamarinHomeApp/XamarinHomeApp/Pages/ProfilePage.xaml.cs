6d7488c7e8c460bc600bcda47ab5abb03a3adace
sTorov <sotorov2011@yandex.ru>
2023-03-03 02:11:14 +0300

    ProfilePage TableView

---
+++ XamarinHomeApp/XamarinHomeApp/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinHomeApp.Pages
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private void NotifyUser(object sender, ToggledEventArgs e)
+        {
+            //Покажем уведомление с предупреждение, если пользователь выдаст сразу все доступы
+            if (climateSwitch.On && gasSwitch.On && electroSwitch.On)
+                DisplayAlert("Внимание!!!", "Пользователь получит полный доступ ко всем системам", "ОК");
+        }
+    }
+}
